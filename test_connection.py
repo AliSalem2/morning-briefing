a5eab6845f10f7c4e24cf788b7ed512d14bb4307
Ali Salem <leesalem@gmail.com>
2026-05-18 14:40:39 +0200

    Remove test file

--- test_connection.py
+++
@@ -1,9 +0,0 @@
-import litellm
-
-response = litellm.completion(
-    model="ollama/phi3:mini",
-    api_base="http://localhost:11434",
-    messages=[{"role": "user", "content": "Say hello in one sentence"}]
-)
-
-print(response.choices[0].message.content)
